0a76dd4e55b2da41d6df2016fd4c589ca8b8c861
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-01-19 10:45:26 -0300

    alteração no arquivo classe conexao.php adicionado banco de dados

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 
 define('SGBD', 'mysql');
 define('HOST', 'localhost');
-define('DBNAME', '');
+define('DBNAME', 'db_blog_master');
 define('CHARSET', 'utf8');
 define('USER', 'root');
 define('PASSWORD', 'root');
