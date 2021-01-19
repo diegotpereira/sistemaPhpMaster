4b1bcb5bfff243a016f6b3ecbfe4642bf42e6c5c
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-01-19 10:33:22 -0300

    criação do arquivo classe conexao.php

---
+++ conexao.php
@@ -0,0 +1,83 @@
+<?php
+
+define('SGBD', 'mysql');
+define('HOST', 'localhost');
+define('DBNAME', '');
+define('CHARSET', 'utf8');
+define('USER', 'root');
+define('PASSWORD', 'root');
+define('SERVER', 'mysql');
+
+class conexao{
+
+    private static $pdo;
+
+
+    private function __construct(){}
+
+    private static function verificaExtensao(){
+
+        switch(SGBD):
+            case'mysql':
+                $extensao = 'pdo_mysql';
+                break;
+            case 'mssql':{
+                if(SERVER=='linux'):
+                    $extensao = 'pdo_dblib';
+                else:
+                    $extensao = 'pdo_sqlsrv';
+                endif;
+                break;    
+            }
+            case 'postgre';
+                 $extensao = 'pdo_pgsql';
+                 break;
+
+        endswitch;     
+
+        if(!extension_loaded($extensao)):
+           echo "<h1>Extensao {$extensao} não habilitada!</h1>";
+           exit();
+        endif;   
+    }
+
+    public static function getInstance() {
+
+        self::verificaExtensao();
+
+        if (!isset(self::$pdo)) {
+            try {
+                $opcoes = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8');
+                switch (SGBD) :
+                    case 'mysql':
+                        self::$pdo = new \PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . ";", USER, PASSWORD, $opcoes);
+                        break;
+                    case 'mssql':{
+                        if(SERVER == 'linux'):
+                            self::$pdo = new \PDO("dblib:host=" . HOST . "; database=" . DBNAME . ";", USER, PASSWORD, $opcoes);
+                        else:
+                            self::$pdo = new \PDO("sqlsrv:server=" . HOST . "; database=" . DBNAME . ";", USER, PASSWORD, $opcoes);
+                        endif;
+                        break;
+                    }
+                    case 'postgre':
+                        self::$pdo = new \PDO("pgsql:host=" . HOST . "; dbname=" . DBNAME . ";", USER, PASSWORD, $opcoes);
+                        break;
+                endswitch;
+                self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                print "Erro: " . $e->getMessage();
+            }
+        }
+        return self::$pdo;
+    }
+    public static function isConectado(){
+        if(self::$pdo):
+            return true;
+        else:
+            return false;
+        endif;        
+    }
+}
+
+?>
